e4c100d9a03ca8471fea63effd195d9b33e16c00
quan <quanximing@honbow.com>
2021-02-08 14:40:37 +0800

    香港

---
+++ backend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'hUq9o14NY5XOnY7ssfWPsqvuJnqf_8p0',
+        ],
+    ],
+];
